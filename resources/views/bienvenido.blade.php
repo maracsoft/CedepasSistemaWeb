c90f1c35355c568c1a61a8e6ee304cd7f04e0010
Vigotes <degolego2000@gmail.com>
2021-03-03 00:04:23 -0500

    Ya arreglado

--- resources/views/bienvenido.blade.php
+++ resources/views/bienvenido.blade.php
@@ -2,11 +2,7 @@
 @section('contenido')
 
 
-<h1> FELIZ ANIVERSARIOOOOOOOOOOOOOOOOOO  </h1>
+<h1> BIENVENIDO </h1>
 
-DIGO, ES UNA SEMANA PERO ES ALGO PUES 
-
-
-<img src="/img/eli.jpeg" alt="" width="40%" height="40%">
 
 @endsection
