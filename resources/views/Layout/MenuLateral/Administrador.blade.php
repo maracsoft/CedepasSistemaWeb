52f55fc3693f12a7b6f67c8c33978df222e4fa54
VigoTes <degolego2000@gmail.com>
2021-03-29 02:19:01 -0500

    cambie gerent x admin en un error

--- resources/views/Layout/MenuLateral/Administrador.blade.php
+++ resources/views/Layout/MenuLateral/Administrador.blade.php
@@ -1,20 +1,20 @@
     <li>- - - - - - -   ADMINISTRADOR   - - - - - - - -</li>
     <li class="nav-item">
-      <a href="{{route('SolicitudFondos.Gerente.Listar')}}" class="nav-link">
+      <a href="{{route('SolicitudFondos.Administrador.Listar')}}" class="nav-link">
         <i class="far fa-address-card nav-icon"></i>
         <p>Abonar Solicitudes</p>
       </a>
     </li>
 
     <li class="nav-item">
-      <a href="{{route('RendicionGastos.Gerente.Listar')}}" class="nav-link">
+      <a href="{{route('RendicionGastos.Administrador.Listar')}}" class="nav-link">
         <i class="far fa-address-card nav-icon"></i>
         <p>Observar Rendiciones</p>
       </a>
     </li>
 
     <li class="nav-item">
-      <a href="{{route('ReposicionGastos.Gerente.Listar')}}" class="nav-link">
+      <a href="{{route('ReposicionGastos.Administrador.Listar')}}" class="nav-link">
         <i class="far fa-address-card nav-icon"></i>
         <p>Abonar Reposiciones</p>
       </a>
